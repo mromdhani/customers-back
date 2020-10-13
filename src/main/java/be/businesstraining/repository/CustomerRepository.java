c9a98d14dbb032be5be1490bb03815bc2e0ec318
Mohamed Romdhani <mromdhani7@gmail.com>
2020-10-13 15:46:32 +0200

    Commit Liste de customers

---
+++ src/main/java/be/businesstraining/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package be.businesstraining.repository;
+
+import java.util.List;
+
+import be.businesstraining.domain.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository
+        extends CrudRepository<Customer, Long> {
+
+
+}
